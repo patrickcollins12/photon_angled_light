6c8c5b0a2f46c56f9cc15ef7c1391396bf5378ff
Patrick Collins <pcollins1@gmail.com>
2019-01-01 01:58:50 -0800

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const webpack = require('webpack'); //to access built-in plugins
+const nodeExternals = require('webpack-node-externals')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 
 module.exports = {
@@ -8,6 +10,15 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css$/, use: 'css-loader' },
+      {
+        // Transpiles ES6-8 into ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
     ]
   }
 
